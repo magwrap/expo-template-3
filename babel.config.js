8cf6c984d4582243841d57ff25df2285b88b0098
magwrap <jasiek.musiol@gmail.com>
2023-06-09 18:11:19 +0200

    absolute path added

--- babel.config.js
+++ babel.config.js
@@ -1,17 +1,16 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo"],
+    presets: ['module:metro-react-native-babel-preset'],
     plugins: [
       "react-native-paper/babel",
-      
       [
         "module-resolver",
         {
           root: ["./"],
           extensions: [".ios.js", ".android.js", ".js", ".ts", ".tsx", ".json"],
           alias: {
-            "@": ["./src"],
+            "@": "./src",
           },
         },
       ],
